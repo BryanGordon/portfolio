001eb9a51534381548c4b05e98df0486e9bffe8e
BryanGordon <bryanbge92@gmail.com>
2024-04-12 18:09:41 -0500

    Update App.jsx
    Replaced CV to PersonalInformation component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Home } from './components/Home'
 import { Header } from './components/Header'
 import { Projects } from './components/Projects'
 import { Footer } from './components/Footer'
-import { Cv } from './components/Cv'
+import { PersonalInformation } from './components/PersonalInformation'
 
 function App () {
   return (
@@ -14,7 +14,7 @@ function App () {
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route exact path='/projects' element={<Projects />} />
-          <Route exact path='/cv' element={<Cv />} />
+          <Route exact path='/personal-information' element={<PersonalInformation />} />
         </Routes>
       </main>
       <Footer />
