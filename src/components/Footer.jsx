7ca8c144c1e7ca00599487d6c6405996faaf02f6
BryanGordon <bryanbge92@gmail.com>
2024-04-17 15:32:18 -0500

    Bug fixed
    - Fixed bug on icons.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,12 +9,14 @@ export function Footer () {
         <a className='link-autor' href='#start'>
           <label className='footer-autor'>Bryan Gordon</label>
         </a>
-        <a className='link-footer-linke' target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/bryan-gordon-9ab303220'>
-          <IconBrandLinkedin />
-        </a>
-        <a className='link-footer-github' target='_blank' rel='noreferrer' href='https://github.com/BryanGordon'>
-          <IconBrandGithub />
-        </a>
+        <div className='icons'>
+          <a className='link-footer-linke' target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/bryan-gordon-9ab303220'>
+            <IconBrandLinkedin />
+          </a>
+          <a className='link-footer-github' target='_blank' rel='noreferrer' href='https://github.com/BryanGordon'>
+            <IconBrandGithub />
+          </a>
+        </div>
       </div>
     </footer>
   )
