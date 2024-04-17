f0b42b5ec5eeb91f811a1c1b3c4935422c97c382
BryanGordon <bryanbge92@gmail.com>
2024-04-17 15:52:47 -0500

    Add icons
    - Added GitHub and LinkedIn icons on header info.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import '../css/Header.css'
+import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react'
 import { Navbar } from './Navbar'
 import imgDev from '../assets/image-cv.jpg'
 
@@ -8,6 +9,14 @@ export function Header () {
       <div className='info-container'>
         <p className='header-info'>
           Hello! My name is Bryan Gordon, front end developer with +2 years of experience developing websites. I'm form Ecuador and I speak Spanish and English.
+          <div className='icon'>
+            <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/bryan-gordon-9ab303220'>
+              <IconBrandLinkedin className='icon-linke' />
+            </a>
+            <a target='_blank' rel='noreferrer' href='https://github.com/BryanGordon'>
+              <IconBrandGithub className='icon-git' />
+            </a>
+          </div>
         </p>
         <div className='img-container-header'>
           <img src={imgDev} alt='Bryan Gordon portfolio image' />
