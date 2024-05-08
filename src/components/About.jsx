3e23b7f84dcf5f1cf4eb915e299ec5d86bf03ee3
BryanGordon <bryanbge92@gmail.com>
2024-05-08 12:20:02 -0500

    Update information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export function About () {
       <h1>About me</h1>
       <div className='container-about'>
         <p>
-          I started studying at the University and then as an autodidact. Been developing software since 2017, in 2021 started with front end development with React, having experience with JavaScript, CSS, HTML. Also developing with other technologies like Axios, Tailwind CSS, TypeScript, Zustand, PostgreSQL, and MySQL.
+          I started studying at the University and then as an autodidact. Been developing software since 2017, in 2021 started with front end development with React, having experience with JavaScript, CSS, HTML and TailwindCSS. Also developing with other technologies like TypeScript, Axios, Zustand, PostgreSQL, and MySQL.
         </p>
         <p>
-          Currently with knowledge in software development, web development, video game development and video game testing. I have +2 years of experience developing all types of websites and web applications, with the aim of providing a good usability and user experience.
+          Currently with knowledge in software development, web development, video game development and video game testing. I have +2.5 years of experience developing all types of websites and web applications, with the aim of providing a good usability and user experience.
         </p>
       </div>
     </section>
