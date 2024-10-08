4efb8b85a3eb78076208430bb23aa4580ab73a71
Bryan Gordon <bryanbge92@gmail.com>
2024-10-08 14:15:09 -0500

    Improved code reading

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,23 +18,29 @@ export function Projects () {
   return (
     <section className='section-projects'>
       <h1>Projects</h1>
+
       <div className='projects-content'>
         <ul className='container-projects'>
           {
+
             data.map((item) => (
               <li className='projects' key={item.titleProject}>
+
                 <div className='img-container-projects'>
                   <a target='_blank' rel='noreferrer' href={item.linkProject}>
                     <img className='image-project' src={item.imageProject} alt={item.projectAlt} />
                   </a>
                 </div>
+
                 <label className='project-title'>{item.titleProject}</label>
                 <p className='description-project'>{item.description}</p>
+
               </li>
             ))
           }
         </ul>
       </div>
+
       <p className='more-info'>
         If you want see all my projects, you can click the icon next to
         <a target='_blank' rel='noreferrer' href='https://github.com/BryanGordon'>
