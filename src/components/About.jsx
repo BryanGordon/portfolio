62a16bc1083b8df12093efb9bee33abe20cf1983
Bryan Gordon <bryanbge92@gmail.com>
2025-01-14 17:29:55 -0500

    Update info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,15 @@ export function About () {
       <h1>About me</h1>
       <div className='container-about'>
         <p>
-          I started studying at the University and then as an autodidact. Been developing software since 2017, in 2021 started with front end development with React, having experience with JavaScript, CSS, HTML and TailwindCSS. Also developing with other technologies like TypeScript, Axios, Zustand, PostgreSQL, and MySQL.
+          I started studying at the University and then as an autodidact. <span className='markBody'>Been developing software since 2017, in 2021 started with front end development with React</span>, having experience with multiple languages for example <span className='markBody'>Java, Python and HTML</span>. Also experience in others technologies/frameworks like <span className='markBody'>Axios, Zustand, PostgreSQL</span>.
         </p>
+
         <p>
-          Currently with knowledge in software development, web development, video game development and video game testing. I have +2.5 years of experience developing all types of websites and web applications, with the aim of providing a good usability and user experience.
+          knowledge in software development, web development, video game development and video game testing. I have <span className='markBody'>+3 years of experience developing</span> all types of <span className='markBody'>websites and web applications</span>, with the aim of providing a good usability and user experience.
+        </p>
+
+        <p>
+          Currently <span className='markBody'>working as free lancer</span> developing scalability web sites for any customers and at the moment <span className='markBody'>my current stack</span> are: <span className='markBody'>TypeScript, JavaScript, CSS, TailwindCSS, React, Astro, NextJS and React Native</span>.
         </p>
       </div>
     </section>
