0c4d8d8e4862441def2bb3f0a9d67f6f74d03c5b
Bryan Gordon <bryanbge92@gmail.com>
2025-01-14 17:30:22 -0500

    Add weight to font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import '../css/Header.css'
 import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react'
 import { Navbar } from './Navbar'
-import imgDev from '../assets/image-cv.jpg'
+import imgDev from '../assets/image-hap-cv.jpg'
 
 export function Header () {
   return (
     <header id='start'>
       <div className='info-container'>
         <p className='header-info'>
-          Hello! My name is Bryan Gordon, front end developer with +2.5 years of experience developing websites. I'm form Ecuador and I speak Spanish and English.
+          Hello! My name is <span>Bryan Gordon</span>, front end developer with <span>+3 years of experience</span> developing websites. I'm form Ecuador and I speak <span>Spanish</span> and <span>English</span>.
           <div className='icon'>
             <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/bryan-gordon-9ab303220'>
               <IconBrandLinkedin className='icon-linke' />
@@ -18,9 +18,10 @@ export function Header () {
             </a>
           </div>
         </p>
-        <div className='img-container-header'>
+
+        <picture className='img-container-header'>
           <img src={imgDev} alt='Bryan Gordon portfolio image' />
-        </div>
+        </picture>
       </div>
       <div className='navbar-container'>
         <Navbar />
